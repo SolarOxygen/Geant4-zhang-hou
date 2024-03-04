631e57842afd8985f24ceb2ebd7e8a6fce474d75
SolarOxygen <158460028+SolarOxygen@users.noreply.github.com>
2024-03-04 16:04:43 +0800

    Add files via upload

---
+++ src/G4STARTDetectorConstruction.hh
@@ -0,0 +1,35 @@
+#ifndef G4STARTDetectorConstruction_h
+#define G4STARTDetectorConstruction_h 1
+
+
+#include "G4VUserDetectorConstruction.hh"
+#include "globals.hh"
+#include "G4Cache.hh"
+#include "tls.hh"
+
+class G4Material;
+class G4MaterialPropertiesTable;
+class G4STARTElectricFieldSetup;
+
+class G4STARTDetectorConstruction : public G4VUserDetectorConstruction{
+public:
+    G4STARTDetectorConstruction();
+    ~G4STARTDetectorConstruction() override;
+
+    G4VPhysicalVolume* Construct() override;
+    void DefineMaterials();
+    virtual void ConstructSDandField() override;
+    //void SetMaxStep (G4double );
+protected:
+    G4Material* diamond;
+    //G4MaterialPropertiesTable* diamondMPT;
+    //G4Material* fPb;
+    G4Material* fW;
+    G4Material* fSi;
+    G4Material* fAl;
+    G4Material* fBe;
+
+private:
+    G4Cache<G4STARTElectricFieldSetup*> fEmFieldSetup;
+};
+#endif
